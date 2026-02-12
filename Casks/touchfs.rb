9d4f47a71b489a8536bdc7137b7261c0c3ccd0e0
Leonard Tan <lentan1029@gmail.com>
2026-02-13 01:02:05 +0800

    Add touchfs cask v1.0.0

---
+++ Casks/touchfs.rb
@@ -0,0 +1,19 @@
+cask "touchfs" do
+  version "1.0.0"
+  sha256 "c366d73995a0ca8695ab3d229cc20ab46c35bf744412cf8ebe6f9cd311194844"
+
+  url "https://github.com/tetratorus/touchfs/releases/download/v#{version}/touchfs.zip"
+  name "touchfs"
+  desc "Touch ID-gated encrypted files"
+  homepage "https://github.com/tetratorus/touchfs"
+
+  depends_on cask: "fuse-t"
+
+  app "touchfs.app"
+
+  binary "#{appdir}/touchfs.app/Contents/MacOS/touchfs"
+
+  uninstall quit: "com.bluzuli.touchfs"
+
+  zap trash: []
+end
