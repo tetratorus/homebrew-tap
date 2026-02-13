0050af2d4f8c44f18051e7eef662c4bc0d6f57e3
Leonard Tan <lentan1029@gmail.com>
2026-02-13 08:44:57 +0800

    Update touchfs to v1.1.0

--- Casks/touchfs.rb
+++ Casks/touchfs.rb
@@ -1,6 +1,6 @@
 cask "touchfs" do
-  version "1.0.10"
-  sha256 "342f29bedb5846afc620ed919bfa51406ae6f0b8ffed6bdec662bd9cea1dae6c"
+  version "1.1.0"
+  sha256 "73d3007466afaa75bfc20766dc5381e706061cad2ffb36bcfe646b77afd30015"
 
   url "https://github.com/tetratorus/touchfs/releases/download/v#{version}/touchfs.zip"
   name "touchfs"
