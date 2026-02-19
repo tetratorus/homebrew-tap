c582783d5806682b450deb094d552b6b94558e15
Leonard Tan <lentan1029@gmail.com>
2026-02-19 19:28:50 +0800

    Update touchfs to v1.6.1

--- Casks/touchfs.rb
+++ Casks/touchfs.rb
@@ -1,6 +1,6 @@
 cask "touchfs" do
-  version "1.6.0"
-  sha256 "0adfb2d096a930f1be03a70837b5e896778c8cecb708e5a6dcd6fbfa846f8b91"
+  version "1.6.1"
+  sha256 "f0661b0a1a80a1f3a9990b4f271211448160b45b033813a881380cdaf8de3fe3"
 
   url "https://github.com/tetratorus/touchfs/releases/download/v#{version}/touchfs.zip"
   name "touchfs"
